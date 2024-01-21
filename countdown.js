d3676b989df8a3687a5886afca65fd678ba4bc79
Personal-pb <134315924+Personal-pb@users.noreply.github.com>
2024-01-21 15:22:17 +0100

    Add files via upload

--- countdown.js
+++ countdown.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("July 1, 2023 07:00:00").getTime();
+var countDownDate = new Date("Jan 1, 2024 07:00:00").getTime();
 var x = setInterval(function(){
     var now = new Date().getTime();
     var distance = countDownDate - now;
